8d209cf303476429d51c652b9f8facec8e74f6e2
Alexander Yosimar Cervantes Pineda <aycp.developer@outlook.com>
2023-07-09 22:48:30 -0600

    componente FormContainer añadido en el componente principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css'
+import FormContainer from './container/form-container/FormContainer';
 
 const App = () => {
 
     return (
         <div className='App'>
-
+            <FormContainer />
         </div>
     );
 
